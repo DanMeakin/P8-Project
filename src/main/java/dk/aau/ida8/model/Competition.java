00e04b5739680b2a9a55c4bb90a0450b8d94b174
HappyFisk <frederiknygart@gmail.com>
2016-03-16 15:36:05 +0100

    Added competition class

--- src/main/java/dk/aau/ida8/model/Competition.java
+++ src/main/java/dk/aau/ida8/model/Competition.java
@@ -1,7 +1,168 @@
 package dk.aau.ida8.model;
 
-/**
- * Created by nicklas on 15-03-16.
- */
+import java.util.*;
+
+
 public class Competition {
+
+    private long id;
+    private CompetitionType type;
+    private TreeSet<Lifter> lifters;
+    private String competitionName;
+    private String address;
+    private Date date;
+    private String clubName;
+    private boolean weightInCompleted;
+    private boolean snatchCompleted;
+    private boolean cleanAndJerkCompleted;
+    private boolean finished;
+
+    public Competition() {
+    }
+
+    public Competition(String competitionName, CompetitionType type, String address, Date date, String clubName, long id) {
+        this.competitionName = competitionName;
+        this.type = type;
+        this.address = address;
+        this.date = date;
+        this.clubName = clubName;
+        this.id = id;
+    }
+
+
+    /**
+     * Adds a new lifter to the competition
+     * @param objectVariables of the lifter
+     */
+    public void addLifterToCompetition(String objectVariables){
+        Lifter lifter = new Lifter(objectVariables);
+        getLifters().add(lifter);
+    }
+
+    /**
+     * Removes a competitor from the list of lifters
+     * @param lifter who is to be removed
+     */
+    public void removeLifterFromCompetition (Lifter lifter){
+        getLifters().remove(getLifters().indexOf(lifter));
+    }
+
+    /**
+     * finds a lister based on his index in the ArrayList lifters
+     * @param index in the lifters ArrayList
+     * @return
+     */
+    public Lifter getLifter(int index){
+        return getLifters().get(index);
+    }
+
+    public void ArrangeStartingOrderOfLifts(){
+        Collections.sort(getLifters(), new Comparator<Lifter>() {
+            @Override public int compare(Lifter l1, Lifter l2) {
+                return l1.nextLift(getSnatches()).getWeight - l2.nextLift(getSnatches).getWeight; // Ascending
+            }
+        });
+    }
+
+
+    public ArrayList<Lifter> arrangeOrderOfLifts(){
+        //find the lowest startWeight
+
+
+        //find the order of the lifters based on weightTry
+
+
+
+        //arrange the
+    }
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public CompetitionType getType() {
+        return type;
+    }
+
+    public void setType(CompetitionType type) {
+        this.type = type;
+    }
+
+    public TreeSet getLifters() {
+        return lifters;
+    }
+
+    public void setLifters(TreeSet lifters) {
+        this.lifters = lifters;
+    }
+
+    public String getCompetitionName() {
+        return competitionName;
+    }
+
+    public void setCompetitionName(String competitionName) {
+        this.competitionName = competitionName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getClubName() {
+        return clubName;
+    }
+
+    public void setClubName(String clubName) {
+        this.clubName = clubName;
+    }
+
+    public boolean isWeightInCompleted() {
+        return weightInCompleted;
+    }
+
+    public void setWeightInCompleted(boolean weightInCompleted) {
+        this.weightInCompleted = weightInCompleted;
+    }
+
+    public boolean isSnatchCompleted() {
+        return snatchCompleted;
+    }
+
+    public void setSnatchCompleted(boolean snatchCompleted) {
+        this.snatchCompleted = snatchCompleted;
+    }
+
+    public boolean isCleanAndJerkCompleted() {
+        return cleanAndJerkCompleted;
+    }
+
+    public void setCleanAndJerkCompleted(boolean cleanAndJerkCompleted) {
+        this.cleanAndJerkCompleted = cleanAndJerkCompleted;
+    }
+
+    public boolean isFinished() {
+        return finished;
+    }
+
+    public void setFinished(boolean finished) {
+        this.finished = finished;
+    }
 }
