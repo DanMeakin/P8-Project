01d66a64cfaf658bd6d80b10842755c100605770
Dan Meakin <dan@danmeakin.com>
2016-03-17 12:29:18 +0100

    Create Participation model class; amend and add to Competition model class.

--- src/main/java/dk/aau/ida8/model/Competition.java
+++ src/main/java/dk/aau/ida8/model/Competition.java
@@ -7,53 +7,97 @@ public class Competition {
 
     private long id;
     private CompetitionType type;
-    private TreeSet<Lifter> lifters;
+    private HashSet<Participation> participations;
     private String competitionName;
-    private String address;
+    private Address location;
+    private Club host;
     private Date date;
-    private String clubName;
     private boolean weightInCompleted;
     private boolean snatchCompleted;
     private boolean cleanAndJerkCompleted;
     private boolean finished;
 
-    public Competition() {
-    }
-
-    public Competition(String competitionName, CompetitionType type, String address, Date date, String clubName, long id) {
+    /**
+     * Creates a new Competition object.
+     *
+     * A competition requires a title, a host club, a location, a competition
+     * type and a date. These are required parameters for the creation of a
+     * Competition.
+     *
+     * @param competitionName the title/name of the competition
+     * @param host            the club hosting the competition
+     * @param location        the venue at which the competition takes place
+     * @param type            the type of competition, e.g. Sinclair, weight
+     *                        classes
+     * @param date            the date on which the competition is to take place
+     */
+    public Competition(String competitionName, Club host, Address location, CompetitionType type, Date date) {
         this.competitionName = competitionName;
         this.type = type;
-        this.address = address;
+        this.location = location;
         this.date = date;
-        this.clubName = clubName;
-        this.id = id;
+        this.host = host;
+        this.participations = new HashSet<>();
     }
 
-
     /**
-     * Adds a new lifter to the competition
-     * @param objectVariables of the lifter
+     * Adds a new participant to the competition.
+     *
+     * The participation of a lifter in a competition is encapsulated within a
+     * {@link Participation Participation} object. This method accepts a lifter
+     * instance and then creates and aggregates the required Participation
+     * object to the competition.
+     *
+     * @param lifter the lifter to add to the competition
      */
-    public void addLifterToCompetition(String objectVariables){
-        Lifter lifter = new Lifter(objectVariables);
-        getLifters().add(lifter);
+    public void addParticipant(Lifter lifter){
+        Participation p = new Participation(lifter, this);
+        participations.add(p);
     }
 
     /**
-     * Removes a competitor from the list of lifters
+     * Removes a participant from the list of participants.
+     *
      * @param lifter who is to be removed
      */
-    public void removeLifterFromCompetition (Lifter lifter){
-        getLifters().remove(getLifters().indexOf(lifter));
+    public void removeParticipant(Lifter lifter){
+        Participation p = selectParticipationByLifter(lifter);
+        removeParticipant(p);
     }
 
     /**
-     * finds a lister based on his index in the ArrayList lifters
-     * @param index in the lifters ArrayList
-     * @return
+     * Removes a participant from the participations list.
+     *
+     * @param participation the participation object to remove
      */
-    public Lifter getLifter(int index){
-        return getLifters().get(index);
+    private void removeParticipant(Participation participation) {
+        participations.remove(participation);
+    }
+
+    /**
+     * Determines the next participant to carry out a lift.
+     *
+     * The identity of the next participant is calculated based on which
+     * participant has chosen the lowest weight to lift. If two or more lifters
+     * are to be lifting the same weight, the next participant is determined
+     * by taking the lowest of these lifters' ID#.
+     *
+     * @return the participation object containing the next lifter who is to
+     *         lift
+     */
+    public Participation determineNextParticipation() {
+        return Collections.min(getParticipations(), new Comparator<Participation>() {
+            @Override
+            public int compare(Participation p1, Participation p2) {
+                int weightComp = p1.getCurrentWeight() - p2.getCurrentWeight();
+                long idComp = p1.getLifter().getId() - p2.getLifter().getId();
+                if (weightComp == 0) {
+                    return (int) idComp;
+                } else {
+                    return weightComp;
+                }
+            }
+        }
     }
 
     public void ArrangeStartingOrderOfLifts(){
@@ -76,16 +120,6 @@ public class Competition {
         //arrange the
     }
 
-
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
     public CompetitionType getType() {
         return type;
     }
@@ -94,12 +128,12 @@ public class Competition {
         this.type = type;
     }
 
-    public TreeSet getLifters() {
-        return lifters;
+    public HashSet<Participation> getParticipations() {
+        return participations;
     }
 
-    public void setLifters(TreeSet lifters) {
-        this.lifters = lifters;
+    public TreeSet getLifters() {
+        return lifters;
     }
 
     public String getCompetitionName() {
@@ -110,12 +144,12 @@ public class Competition {
         this.competitionName = competitionName;
     }
 
-    public String getAddress() {
-        return address;
+    public Address getLocation() {
+        return this.location;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    public void setLocation(Address location) {
+        this.location = location;
     }
 
     public Date getDate() {
@@ -126,12 +160,12 @@ public class Competition {
         this.date = date;
     }
 
-    public String getClubName() {
-        return clubName;
+    public Club getHost() {
+        return host;
     }
 
-    public void setClubName(String clubName) {
-        this.clubName = clubName;
+    public void setHost(Club host) {
+        this.host = host;
     }
 
     public boolean isWeightInCompleted() {
---
+++ src/main/java/dk/aau/ida8/model/Participation.java
@@ -0,0 +1,142 @@
+package dk.aau.ida8.model;
+
+import org.omg.CORBA.DynAnyPackage.Invalid;
+
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class represents the participation of one lifter in a competition. It
+ * contains methods and attributes related to the performance of a lifter within
+ * a competition.
+ *
+ * A lifter undertakes six different lifts within a competition: three of these
+ * are snatch, and three are clean & jerk. Lifts are aggregated to a
+ * participation instance. Logic is found within the participation class to
+ * handle these constraints.
+ *
+ * It should not be possible to carry-out more than six lifts, and it should not
+ * be possible to carry-out more than three lifts of each type.
+ */
+public class Participation {
+
+    private Lifter lifter;
+    private Competition competition;
+    private int currentWeight;
+    private List<Lift> lifts;
+
+    /**
+     * Gets the list of all lifts undertaken during this participation.
+     *
+     * @return list of all lifts undertaken
+     */
+    public List<Lift> getLifts() {
+        return lifts;
+    }
+
+    /**
+     * Creates a participation instance.
+     *
+     * @param lifter         the lifter participating in a competition
+     * @param competition    the competition in which the lifter is
+     *                       participating
+     * @param startingWeight the initial weight to be lifted by the lifter
+     */
+    public Participation(Lifter lifter, Competition competition, int startingWeight) {
+        this.lifter = lifter;
+        this.competition = competition;
+        this.currentWeight = startingWeight;
+        this.lifts = new ArrayList<>();
+    }
+
+    public Lifter getLifter() {
+        return lifter;
+    }
+
+    public Competition getCompetition() {
+        return competition;
+    }
+
+    /**
+     * Gets the current chosen weight to lift.
+     *
+     * @return current weight
+     */
+    public int getCurrentWeight() {
+        return currentWeight;
+    }
+
+    /**
+     * Sets the weight this participant chooses to lift next.
+     *
+     * Weight can only increase. As such, this method ensures that the new
+     * weight is not less than the current weight. If the new weight is lower
+     * than existing, an IllegalArgumentException is thrown.
+     *
+     * @param newWeight the weight this lifter will be lifting next
+     */
+    public void setCurrentWeight(int newWeight) throws IllegalArgumentException {
+        if (this.currentWeight >= newWeight) {
+            String msg = "unable to set new weight to " + newWeight + "kg; " +
+                         "current weight of " + this.currentWeight + " kg " +
+                         "is greater than new weight";
+            throw new IllegalArgumentException(msg);
+        }
+        this.currentWeight = newWeight;
+    }
+
+
+    /**
+     * Creates and adds a lift to a participation instance.
+     *
+     * An InvalidParameterException will be thrown if the lift is of a type
+     * which has been fully completed.
+     *
+     * @param activity   the type of lift undertaken
+     * @param successful flag whether this particular lift was successful or
+     *                   not
+     */
+    public void addLift(String activity, boolean successful) throws InvalidParameterException {
+        Lift lift = new Lift(getCurrentWeight(), activity);
+        lift.setPerformed(true);
+        lift.setAccepted(successful);
+        addLift(lift);
+    }
+
+    /**
+     * Adds a lift to a participation instance.
+     *
+     * @param lift
+     */
+    private void addLift(Lift lift) throws InvalidParameterException {
+        validateLiftConditions(lift);
+        lifts.add(lift);
+    }
+
+    /**
+     * Validates that a lift which is to be added to this participation is valid.
+     *
+     * A lift is considered to be valid where fewer than six lifts in total have
+     * been carried out as part of this participation, and where fewer than
+     * three lifts of the same type as that of the lift passed have been
+     * undertaken.
+     *
+     * This method throws an exception if the lift conditions are invalid. If
+     * they are valid, no exception is thrown.
+     *
+     * @param lift the lift which is to be checked for validity
+     */
+    private void validateLiftConditions(Lift lift) throws InvalidParameterException {
+        if (getLifts().size() >= 6) {
+            String msg = "unable to add lift: six lifts already completed";
+            throw new InvalidParameterException(msg);
+        } else {
+            // We need extra logic in here to compare lift activity with the
+            // activities already listed. The lift class does not contain any
+            // details of the activity undertaken during a lift, so this needs
+            // to be added before we can complete this.
+            return;
+        }
+    }
+}
