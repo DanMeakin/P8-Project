4128a10d7ba669975bcfa4789bb8cae9e8d188d1
Dan Meakin <dan@danmeakin.com>
2016-04-12 12:55:05 +0200

    Add lift register form routes to CompetitionController.

--- src/main/java/dk/aau/ida8/controller/CompetitionController.java
+++ src/main/java/dk/aau/ida8/controller/CompetitionController.java
@@ -2,11 +2,16 @@ package dk.aau.ida8.controller;
 
 import dk.aau.ida8.service.LifterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/competition")
 public class CompetitionController {
 
     private LifterService lifterService;
@@ -23,10 +28,26 @@ public class CompetitionController {
     }
     */
 
-    @RequestMapping("/competition")
+    @RequestMapping(method = RequestMethod.GET)
     public String showAll(Model model) {
         model.addAttribute("lifters", lifterService.findAll());
         return "competition";
+    }
+
+    @RequestMapping(value = "/{competitionID}/register-lift/{participantID}", method = RequestMethod.GET)
+    public String liftOutcomeForm(Model model, @PathVariable long competitionID, @PathVariable long participantID) {
+        return "lift-register-form";
+    }
+
+    @RequestMapping(value = "/{competitionID}/register-lift/{participantID}", method = RequestMethod.POST, produces = MediaType.TEXT_HTML_VALUE)
+    public String registerLift(Model model, @RequestParam("action") String action, @PathVariable long competitionID, @PathVariable long participantID) {
+        if (action == "PASS") {
+            return "<div>It's a pass!</div>";
+        } else if (action == "FAIL") {
+            return "<div>It's a fail!</div>";
+        } else {
+            return "<div>It's an abstention!</div>";
+        }
     }
 
 }
