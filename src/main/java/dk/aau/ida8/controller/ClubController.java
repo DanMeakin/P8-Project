af22341af6033de65d74125bee44133c3c6c971c
doolylol <n.jrg89@gmail.com>
2016-04-27 12:43:48 +0200

    typos

--- src/main/java/dk/aau/ida8/controller/ClubController.java
+++ src/main/java/dk/aau/ida8/controller/ClubController.java
@@ -7,10 +7,7 @@ import dk.aau.ida8.service.LifterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
