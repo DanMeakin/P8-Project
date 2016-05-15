2c2c6c3f852a84cbcd413df87f3c3bf5e0d2602a
Dan Meakin <dan@danmeakin.com>
2016-05-15 12:18:14 +0200

    Increasing Javadoc coverage for Club & Competition controllers; update Competition controllers to ensure accessibility only when appropriate; minor refactoring.

--- src/main/java/dk/aau/ida8/controller/ClubController.java
+++ src/main/java/dk/aau/ida8/controller/ClubController.java
@@ -11,19 +11,45 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
+/**
+ * This class is the controller for all club-centric activities within this
+ * software.
+ *
+ * The ClubController exposes routes relating to the creation, updating and
+ * removal of lifter members of a club. At present, there is no functionality
+ * relating to the creation or removal of clubs themselves.
+ */
 @Controller
+@RequestMapping("/club")
 public class ClubController {
 
     private ClubService clubService;
     private LifterService lifterService;
 
+    /**
+     * Instantiates a ClubController.
+     *
+     * Uses the @Autowired annotation to automatically create instances of the
+     * required services. This allows ORM access as required without manually
+     * creating these services.
+     *
+     * @param clubService   the service used to access Club data
+     * @param lifterService the service used to access Lifter data
+     */
     @Autowired
     public ClubController(ClubService clubService, LifterService lifterService) {
         this.clubService = clubService;
         this.lifterService = lifterService;
     }
 
-    @RequestMapping("/club/new-lifter")
+    /**
+     * Displays the new lifter view for creating a lifter in a particular club.
+     *
+     * @param id    the club ID#
+     * @param model the Spring model object to pass to the view
+     * @return      the new lifter form view
+     */
+    @RequestMapping("/new-lifter")
     public String newLifter(@RequestParam(value = "id", required = false, defaultValue = "1") Long id, Model model) {
         Club currentClub = clubService.findOne(id);
 
@@ -34,7 +60,21 @@ public class ClubController {
         return "club-lifters";
     }
 
-    @RequestMapping(value="/club/save", method = RequestMethod.POST)
+    /**
+     * Saves a new lifter based on details passed to the form in the new lifter
+     * view (see {@link #newLifter(Long, Model) newLifter}).
+     *
+     * The lifter is associated with the club represented by the ID# passed as
+     * parameter to this method.
+     *
+     * No validation of input data is carried out at present. This may lead to
+     * incorrect/invalid data being stored within the system.
+     *
+     * @param id     the club ID#
+     * @param lifter the new lifter to create and associate with club
+     * @return       redirect to {@link #newLifter(Long, Model) new lifter} view
+     */
+    @RequestMapping(value="/save", method = RequestMethod.POST)
     public String saveLifter(@RequestParam(value = "lifter-club-id", required = false, defaultValue = "1") Long id, Lifter lifter) {
         Club currentClub = clubService.findOne(id);
         lifter.setClub(currentClub);
@@ -44,7 +84,17 @@ public class ClubController {
         return "redirect:/club/new-lifter";
     }
 
-    @RequestMapping("/club/remove/{id}")
+    /**
+     * Removes a lifter from a club.
+     *
+     * This method removes the association between a lifter and a club. It does
+     * not delete the lifter him/herself from the system. This permits a lifter
+     * to move to a new club, retaining their profile.
+     *
+     * @param id the ID# of the lifter to remove
+     * @return   redirect to {@link #newLifter(Long, Model) new lifter} view
+     */
+    @RequestMapping("/remove/{id}")
     public String removeLifter(@PathVariable Long id) {
         Lifter lifter = lifterService.findOne(id);
         Club club = clubService.findByName(lifter.getClubName());
@@ -55,13 +105,31 @@ public class ClubController {
         return "redirect:/club/new-lifter";
     }
 
-    @RequestMapping("/club/lifter/{id}")
+    /**
+     * Displays the edit lifter view for modifying an existing lifter within a
+     * club.
+     *
+     * @param id    the ID# of the lifter to edit
+     * @param model the Spring model object to pass to the view
+     * @return      the edit lifter view
+     */
+    @RequestMapping("/lifter/{id}")
     public String updateLifter(@PathVariable Long id, Model model) {
         model.addAttribute("lifter", lifterService.findOne(id));
         return "edit-lifter";
     }
 
-    @RequestMapping(value="club/lifter/save", method = RequestMethod.POST)
+    /**
+     * Updates a lifter's details based on what is passed to the form in the
+     * {@link #updateLifter(Long, Model) update lifter} view.
+     *
+     * No validation of input data is carried out at present. This may lead to
+     * incorrect/invalid data being stored within the system.
+     *
+     * @param lifter the lifter to update
+     * @return       redirect to {@link #newLifter(Long, Model) new lifter} view
+     */
+    @RequestMapping(value="/lifter/save", method = RequestMethod.POST)
     public String saveUpdatedLifter(Lifter lifter) {
         Club club = clubService.findByName(lifter.getClubName());
         lifterService.saveLifter(lifter);
--- src/main/java/dk/aau/ida8/controller/CompetitionController.java
+++ src/main/java/dk/aau/ida8/controller/CompetitionController.java
@@ -11,6 +11,13 @@ import org.springframework.web.bind.annotation.*;
 import java.security.InvalidParameterException;
 import java.util.*;
 
+/**
+ * This class is the controller for all competition-centric activities within
+ * this software.
+ *
+ * The CompetitionController exposes routes relating to the creation and
+ * management of competitions, and the viewing of their results.
+ */
 @Controller
 @RequestMapping("/competition")
 public class CompetitionController {
@@ -21,6 +28,19 @@ public class CompetitionController {
     private ClubService clubService;
     private AddressService addressService;
 
+    /**
+     * Instantiates a CompetitionController.
+     *
+     * Uses the @Autowired annotation to automatically create instances of the
+     * required services. This allows ORM access as required without manually
+     * creating these services.
+     *
+     * @param lifterService      the service used to access Lifter data
+     * @param competitionService the service used to access Competition data
+     * @param participantService the service used to access Participant data
+     * @param clubService        the service used to access Club data
+     * @param addressService     the service used to access Address data
+     */
     @Autowired
     public CompetitionController(LifterService lifterService,
                                  CompetitionService competitionService,
@@ -35,7 +55,8 @@ public class CompetitionController {
     }
 
     /**
-     * Controller method to create a new competition object when on the specified URL
+     * Displays the create competition view for creating a new competition.
+     *
      * @param model the Spring model object to pass to the view
      * @return      the new competition form view
      */
@@ -63,7 +84,13 @@ public class CompetitionController {
         return "redirect:/";
     }
 
-
+    /**
+     * Displays the competition overview for a given competition.
+     *
+     * @param model         the Spring model object to pass to the view
+     * @param competitionID the ID# of the competition to view
+     * @return              the competition overview view
+     */
     @RequestMapping("/{competitionID}")
     public String competitionOverview(Model model, @PathVariable long competitionID) {
         Competition competition = competitionService.findOne(competitionID);
@@ -71,21 +98,51 @@ public class CompetitionController {
         return "competition-overview";
     }
 
+    /**
+     * Displays the competition dashboard for a given competition.
+     *
+     * The dashboard is used to manage the conduct of the competition itself:
+     * the lifts complete, which lifter is next, etc. It is only accessible
+     * following completion of the weigh-in, and before completion of all
+     * lifts.
+     *
+     * If the competition is complete, this controller will redirect to the
+     * {@link #viewRankingGroups(Model, long) results} view. Otherwise, it will
+     * redirect to the {@link #competitionOverview(Model, long) overview} view.
+     *
+     * @param model         the Spring model object to pass to the view
+     * @param competitionID the ID# of the competition to view
+     * @return              the competition dashboard view
+     */
     @RequestMapping("/{competitionID}/dashboard")
     public String competitionDashboard(Model model, @PathVariable long competitionID) {
         Competition competition = competitionService.findOne(competitionID);
         model.addAttribute("competition", competition);
 
-        Optional<Group> currGroup = competition.getCurrentCompetingGroup();
-        if (currGroup.isPresent()) {
+        if (competition.isCompetitionStarted()) {
+            Optional<Group> currGroup = competition.getCurrentCompetingGroup();
             model.addAttribute("participants", currGroup.get().getParticipants());
             model.addAttribute("currParticipant", competition.currentParticipant());
             return "competition-dashboard";
-        } else {
+        } else if (competition.isCompetitionComplete()) {
             return "redirect:/competition/" + competitionID + "/results";
+        } else {
+            return "redirect:/competition/" + competitionID;
         }
     }
 
+    /**
+     * Displays the sign-up view for a given competition.
+     *
+     * This view is only accessible prior to completion of sign-up. If sign-up
+     * is complete, this controller redirects to the
+     * {@link #competitionOverview(Model, long) overview} view.
+     *
+     * @param id            the ID# of the club to sign-up
+     * @param model         the Spring model object to pass to the view
+     * @param competitionID the ID# of the competition to view
+     * @return              the competition dashboard view
+     */
     @RequestMapping("/{competitionID}/sign-up")
     public String competitionSignup(@RequestParam(value = "id", required = false, defaultValue = "1") Long id, Model model, @PathVariable long competitionID) {
         Competition competition = competitionService.findOne(competitionID);
@@ -98,39 +155,88 @@ public class CompetitionController {
         return "competition-sign-up";
     }
 
+    /**
+     * Signs-up a lifter to participant in a competition.
+     *
+     * @param id            the ID# of the lifter to sign-up
+     * @param competitionID the ID# of the competition to sign-up a lifter to
+     * @return              redirect to the
+     *                      {@link #competitionSignup(Long, Model, long) sign-up}
+     *                      view
+     */
     @RequestMapping(value = "/{competitionID}/sign-up", method = RequestMethod.POST)
     public String signupLifterToCompetition(@RequestParam(value = "id", required = false) Long id, @PathVariable long competitionID) {
         Competition competition = competitionService.findOne(competitionID);
         Lifter lifter = lifterService.findOne(id);
         competition.addParticipant(lifter);
         competitionService.save(competition);
-        return "redirect:/competition/" + competition.getId() + "/sign-up";
+        return "redirect:/competition/" + competitionID + "/sign-up";
     }
 
+    /**
+     * Removes a lifter from participation in a competition.
+     *
+     * @param id            the ID# of the lifter to sign-up
+     * @param competitionID the ID# of the competition to sign-up a lifter to
+     * @return              redirect to the
+     *                      {@link #competitionSignup(Long, Model, long) sign-up}
+     *                      view
+     */
     @RequestMapping(value= "/{competitionID}/remove", method = RequestMethod.POST)
     public String removeLifterFromCompetition(@RequestParam(value = "id", required = false) Long id, @PathVariable long competitionID) {
         Competition competition = competitionService.findOne(competitionID);
         Lifter lifter = lifterService.findOne(id);
         competition.removeParticipant(lifter);
         competitionService.save(competition);
-        return "redirect:/competition/" + competition.getId() + "/signup";
+        return "redirect:/competition/" + competitionID + "/sign-up";
     }
 
-    @RequestMapping(value = "/{competitionID}/weigh-in", method = RequestMethod.GET)
+    /**
+     * Displays the competition weigh-in view for a given competition.
+     *
+     * This view is only accessible prior to completion of sign-up. If sign-up
+     * is complete, this controller redirects to the
+     * {@link #competitionOverview(Model, long) overview} view.
+     *
+     * @param model         the Spring model object to pass to this view
+     * @param competitionID the ID# of the competition for which to weigh-in
+     *                      participants
+     * @return              display the weigh-in view
+     */
+    @RequestMapping("/{competitionID}/weigh-in")
     public String controlWeighInParticipants(Model model, @PathVariable long competitionID) {
         Competition competition = competitionService.findOne(competitionID);
-        model.addAttribute("competition", competition);
-        model.addAttribute("participants", competition.getParticipants());
-        return "competition-weigh-in";
+        if (competition.isWeighInStarted()) {
+            model.addAttribute("competition", competition);
+            model.addAttribute("participants", competition.getParticipants());
+            return "competition-weigh-in";
+        } else {
+            return "redirect:/competition/" + competitionID;
+        }
     }
 
+    /**
+     * Attempts to weigh-in a participant with the provided details.
+     *
+     * This method validates that all input values are correct. If they are not,
+     * this method will return a 400 error with details of the error in the
+     * response body. Otherwise, returns a 200 code.
+     *
+     * @param model         the Spring model
+     * @param participantID the ID# of the participant to weigh-in
+     * @param bwStr         the string value of the input bodyweight
+     * @param snatchStr     the string value of the input starting snatch
+     * @param cjStr         the string value of the input starting clean & jerk
+     * @return              JSON response with 400 code if invalid data is
+     *                      input, else 200 if all valid and weigh-in complete
+     */
     @ResponseBody
     @RequestMapping (value = "/{competitionID}/weigh-in/check-in", method = RequestMethod.POST)
     public String checkInParticipant(Model model,
                                      @RequestParam("participantID") long participantID,
-                                     @RequestParam("bodyWeight") String collectedBodyWeight,
-                                     @RequestParam("startingSnatch") String collectedStartingSnatch,
-                                     @RequestParam("startingCJ") String collectedStartingCJ){
+                                     @RequestParam("bodyWeight") String bwStr,
+                                     @RequestParam("startingSnatch") String snatchStr,
+                                     @RequestParam("startingCJ") String cjStr){
 
         Participant participant = participantService.findOne(participantID);
         HashMap<String, String> map = new HashMap<>();
@@ -138,25 +244,25 @@ public class CompetitionController {
 
         try {
             //parsing data received from view into expected types
-            double bodyWeight = Double.parseDouble(collectedBodyWeight);
-            int startingSnatchWeight = Integer.parseInt(collectedStartingSnatch);
-            int startingCleanAndJerkWeight = Integer.parseInt(collectedStartingCJ);
+            double bodyWeight = Double.parseDouble(bwStr);
+            int startingSnatchWeight = Integer.parseInt(snatchStr);
+            int startingCleanAndJerkWeight = Integer.parseInt(cjStr);
 
             //changing model according to values inserted by user
             participant.weighIn(bodyWeight, startingSnatchWeight, startingCleanAndJerkWeight);
 
-            //on succes send succes message with code 200
+            //on success send success message with code 200
             map.put("code", "200");
             map.put("msg", "All good, participant checked in!");
 
         } catch (NumberFormatException e) {
-            String msg = "unable to process input starting Snatch '" + collectedStartingSnatch + "' or starting Clean & Jerk '"
-                    + collectedStartingCJ + "' (a number is required)";
+            String msg = "unable to process input starting Snatch '" + snatchStr + "' or starting Clean & Jerk '"
+                    + cjStr + "' (a number is required)";
             map.put("msg", msg);
             map.put("code", "400");
         } catch (InvalidParameterException e) {
-            String msg = "unable to process input starting Snatch '" + collectedStartingSnatch + "' or starting Clean & Jerk '"
-                    + collectedStartingCJ + "' (the first lift must be greater than 0)";
+            String msg = "unable to process input starting Snatch '" + snatchStr + "' or starting Clean & Jerk '"
+                    + cjStr + "' (the first lift must be greater than 0)";
             map.put("msg", msg);
             map.put("code", "400");
         }
@@ -166,6 +272,17 @@ public class CompetitionController {
         return gson.toJson(map);
     }
 
+    /**
+     * Reverses a weigh-in of a participant.
+     *
+     * This is to be used where, for example, a participant has been erroneously
+     * weighed-in, and this must be reversed.
+     *
+     * @param model         the Spring model
+     * @param participantID the ID# of the participant for whom to reverse
+     *                      weigh-in
+     * @return              JSON response detailing result (should be 200)
+     */
     @ResponseBody
     @RequestMapping(value = "/{competitionID}/weigh-in/check-out", method = RequestMethod.POST)
     public String checkOutParticipant(Model model,
@@ -185,31 +302,70 @@ public class CompetitionController {
         return gson.toJson(map);
     }
 
+    /**
+     * Completes the weigh-in process for a competition.
+     *
+     * TODO: rename and refactor this so it is clearer this completes the weigh-
+     *       in process
+     *
+     * @param model         the Spring model
+     * @param competitionID the ID# of the competition for which to complete
+     *                      weigh-in
+     * @return              redirects to the
+     *                      {@link #viewCompetingGroups(Model, long) competing groups}
+     *                      view
+     */
     @RequestMapping(value = "/{competitionID}/competing-groups", method = RequestMethod.POST)
     public String weighInParticipants(Model model, @PathVariable long competitionID) {
         Competition competition = competitionService.findOne(competitionID);
         competition.finishWeighIn();
         competitionService.save(competition);
-
-        model.addAttribute("competingGroups", competition.getCompetingGroups());
-        model.addAttribute("competition", competition);
-
-        return "competition-groups";
+        return "redirect:/competition/" + competitionID + "/competing-groups";
     }
 
-    @RequestMapping(value = "/{competitionID}/competing-groups", method = RequestMethod.GET)
+    /**
+     * Displays the competing groups view.
+     *
+     * This should only be visible after completion of check-in. If this is not
+     * the case, this controller redirects to the
+     * {@link #competitionOverview(Model, long) overview} view.
+     * @param model         the Spring model to pass to the view
+     * @param competitionID the ID# of the competition for which to display the
+     *                      competing groups
+     * @return              the competing groups view
+     */
+    @RequestMapping("/{competitionID}/competing-groups")
     public String viewCompetingGroups(Model model, @PathVariable long competitionID) {
         Competition competition = competitionService.findOne(competitionID);
-        model.addAttribute("competingGroups", competition.getCompetingGroups());
-        model.addAttribute("competition", competition);
-        return "competition-groups";
+        if (competition.isWeighInComplete()) {
+            model.addAttribute("competingGroups", competition.getCompetingGroups());
+            model.addAttribute("competition", competition);
+            return "competition-groups";
+        } else {
+            return "redirect:/competition/" + competitionID;
+        }
     }
 
-    @RequestMapping(value = "/{competitionID}/results", method = RequestMethod.GET)
+    /**
+     * Displays the results groups view.
+     *
+     * This should only be visible after completion of the competition. If this
+     * is not the case, this controller redirects to the
+     * {@link #competitionOverview(Model, long) overview} view.
+     * @param model         the Spring model to pass to the view
+     * @param competitionID the ID# of the competition for which to display the
+     *                      results groups
+     * @return              the results groups view
+     */
+    @RequestMapping("/{competitionID}/results")
     public String viewRankingGroups(Model model, @PathVariable long competitionID) {
         Competition competition = competitionService.findOne(competitionID);
-        model.addAttribute("rankingGroups", competition.getRankingGroups());
-        model.addAttribute("competition", competition);
-        return "ranking-groups";
+        if (competition.isCompetitionComplete()) {
+            model.addAttribute("rankingGroups", competition.getRankingGroups());
+            model.addAttribute("competition", competition);
+            return "ranking-groups";
+        } else {
+            return "redirect:/competition/" + competitionID;
+        }
     }
 }
